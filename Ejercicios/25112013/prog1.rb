1df348ec11607349fce17e928eb74cd0ef8bcff3
josecolella <josecolella@yahoo.com>
2013-11-29 11:08:42 +0100

    Primero programa en ruby

--- Ejercicios/25112013/prog1.rb
+++ Ejercicios/25112013/prog1.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
+#Author: Jose Miguel Colella
+#Crear un programa en Ruby que imprima los numeros desde 1 hasta otro contenido en una variable
 
-puts "Bienvenido a Ruby"
+def foo(endVariable)
+    (1..endVariable).each do |i|
+        puts "Valor es: #{i}"
+    end
+end
+
+puts "Lo siguiente imprime desde el 1 hasta el 5"
+foo(5)
+puts "Lo siguiente imprime desde el 1 hasta el 10"
+foo(10)
+
+
