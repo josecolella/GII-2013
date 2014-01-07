f60d0c3a0ef10a9596319a01666da387b1407e82
josecolella <josecolella@yahoo.com>
2014-01-07 18:04:54 +0100

    Arreglado unas cosas

--- Ejercicios/13122013/Ejercicio10.py
+++ Ejercicios/13122013/Ejercicio10.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Jose Miguel Colella
 # https://github.com/WindowsAzure/azure-sdk-for-python
@@ -22,7 +21,7 @@ def getContainersWithBlobs(blob_service):
     for i in blob_service.list_containers().containers:
         print("Nombre del contenedor: {}".format(i.name))
         print("Url del contenedor: {}".format(i.url))
-        print("------------------------------")
+        print("##############################")
         for j in blob_service.list_blobs(i.name).blobs:
             print("\tNombre del Blob: {}".format(j.name))
             print("\tUrl del Blob: {}".format(j.url))
@@ -30,10 +29,19 @@ def getContainersWithBlobs(blob_service):
 
 blob_service = BlobService(credentials.account_name, credentials.account_key)
 
+blob_service.delete_blob('code', 'f_blob.py')
 getContainersWithBlobs(blob_service)
 
+
 f_blob = open('Ejercicio10.txt', "w")
-f_blob.write(getContainersWithBlobs(blob_service))
+for i in blob_service.list_containers().containers:
+        f_blob.write("Nombre del contenedor: {}".format(i.name))
+        f_blob.write("Url del contenedor: {}".format(i.url))
+        f_blob.write("##############################")
+        for j in blob_service.list_blobs(i.name).blobs:
+            f_blob.write("\tNombre del Blob: {}".format(j.name))
+            f_blob.write("\tUrl del Blob: {}".format(j.url))
+            f_blob.write("\t------------------------------")
 f_blob.close()
 
-blob_service.put_blob('code', 'f_blob.py', f_blob, 'BlockBlob')
+blob_service.put_blob('code', 'f_blob.txt', f_blob, 'BlockBlob')
