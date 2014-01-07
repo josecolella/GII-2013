1e96ac81c93b229de02c85c76cdaa27a9eaa69ff
josecolella <josecolella@yahoo.com>
2014-01-07 17:47:41 +0100

    Ejercicio 10

---
+++ Ejercicios/13122013/Ejercicio10.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Jose Miguel Colella
+# https://github.com/WindowsAzure/azure-sdk-for-python
+
+# Desde un programa en Ruby o en algún otro lenguaje,
+# listar los blobs que hay en un contenedor,
+# crear un fichero con la lista de los mismos y
+# subirla al propio contenedor.
+
+import credentials  # Contiene información sensible
+from azure.storage import BlobService
+
+
+def getContainersWithBlobs(blob_service):
+    """
+    Imprime los contenedores del usuario con los correspondientes
+    blobs.
+
+    blob_service: Nombre del servicio de gestion de blobs @class BlobService
+    """
+    for i in blob_service.list_containers().containers:
+        print("Nombre del contenedor: {}".format(i.name))
+        print("Url del contenedor: {}".format(i.url))
+        print("------------------------------")
+        for j in blob_service.list_blobs(i.name).blobs:
+            print("\tNombre del Blob: {}".format(j.name))
+            print("\tUrl del Blob: {}".format(j.url))
+            print("\t------------------------------")
+
+blob_service = BlobService(credentials.account_name, credentials.account_key)
+
+getContainersWithBlobs(blob_service)
+
+f_blob = open('Ejercicio10.txt', "w")
+f_blob.write(getContainersWithBlobs(blob_service))
+f_blob.close()
+
+blob_service.put_blob('code', 'f_blob.py', f_blob, 'BlockBlob')
---
+++ Ejercicios/13122013/credentials.py
@@ -0,0 +1,4 @@
+#Informacion necesaria para acceder a azure
+
+account_name = 'ivphotostorage'
+account_key = 'rMWuWANATzFGIyA/7gSc1C19fzP63f3guTs6hYTtDHggHxHA8Cjj2V1HQ1vhtO6G9ZlNYTdE8WshXtdWaq1FLw=='
